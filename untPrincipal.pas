e8a8fc094a6af72b47df355ad99c5ce9a77a91f6
Gabriel Santos <Gabriel.developer.santos@gmail.com>
2020-01-22 07:42:24 -0300

    Primeiro commit do segundo dia - ajuste de layout tela principal

--- untPrincipal.pas
+++ untPrincipal.pas
@@ -4,10 +4,31 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts,
+  FMX.ListBox, FMX.StdCtrls, FMX.Objects, FMX.Controls.Presentation;
 
 type
   TfrmPrincipal = class(TForm)
+    Rectangle1: TRectangle;
+    Layout1: TLayout;
+    Label4: TLabel;
+    imgNoticacao: TImage;
+    cNotificacao: TCircle;
+    btnSair: TSpeedButton;
+    Layout2: TLayout;
+    imgAnterior: TImage;
+    imgProximo: TImage;
+    lblMes: TLabel;
+    lytCalendario: TLayout;
+    lytSemCompromisso: TLayout;
+    Image3: TImage;
+    lblSemCompromisso: TLabel;
+    imgDica: TImage;
+    ltyComCompromisso: TLayout;
+    lbCompromisso: TListBox;
+    Layout3: TLayout;
+    lblDia: TLabel;
+    imgCadCompromisso: TImage;
   private
     { Private declarations }
   public
